54569eeb38e78876c370d161fe7398fbe0b710d4
Andy Li <ali60@stuy.edu>
2025-09-17 11:22:00 -0400

    Created MatrixIntro.py and added return row/column methods

---
+++ MatrixIntro.py
@@ -0,0 +1,13 @@
+# Matrices as Lists of Lists
+# A simple introduction to handling matrices as lists of lists in Python
+# Andy Li
+
+import copy
+from tabulate import tabulate
+
+def returnRow(M, row):
+  return M[row]
+
+def returnCol(M, col):
+  return M[col]
+
