eab07858960e62216812b13e648f3861cc0e8879
Andy Li <ali60@stuy.edu>
2025-09-17 18:24:31 -0400

    fixed errors in code and coded RREF; works for one tested example

--- MatrixIntro.py
+++ MatrixIntro.py
@@ -63,26 +63,27 @@ def REF(M):
             tempRowPointer = -1
             scalarRow(M, rowPointer, M[rowPointer][colPointer] ** (-1))
             for j in range(rowPointer + 1, len(M)):
-                sumRow(M, rowPointer, j, M[i][colPointer] * (-1))
+                sumRow(M, rowPointer, j, M[j][colPointer] * (-1))
         rowPointer += 1
         colPointer += 1
-    for i in range(len(M)):
-        for j in range(len(M[0])):
-            M[i][j] = round(M[i][j], 1)
-    for i in range(len(M) - 1, -1):
-        for j in range(len(M[0])):
-            if M[i][j] != 0:
-                break
-            else:
-                if j == len(M[0]) - 1 and i > 0:
-                    interchange(M, i, i-1)
 
 def RREF(M):
     REF(M)
+    rowPointer = len(M) - 1
+    while rowPointer >= 0:
+        pivotPointer = -1
+        for i in range(len(M[0])):
+            if M[rowPointer][i] != 0:
+                pivotPointer = i
+                break
+        if pivotPointer != -1:
+            for i in range(0, rowPointer):
+                sumRow(M, rowPointer, i, M[i][pivotPointer] * -1)
+        rowPointer -= 1
 
-r_1 = [0, 0, 0]
-r_2 = [3, 6, 0]
-r_3 = [0, 0, 0]
+r_1 = [1, 2, -1, -4]
+r_2 = [-2, 4, -1, 6]
+r_3 = [2, 2, 3, 1]
 M = [r_1, r_2, r_3]
-REF(M)
+RREF(M)
 print(tabulate(M))
