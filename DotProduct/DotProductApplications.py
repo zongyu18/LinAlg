14ff031981fb5470b4d9c9193cc897d1781cc605
Andy Li <ali60@stuy.edu>
2026-01-15 20:39:41 -0500

    finished creating vectors based on nutritional values

--- DotProduct/DotProductApplications.py
+++ DotProduct/DotProductApplications.py
@@ -15,4 +15,22 @@ for index, row in fruits.iterrows():
         new_current_fruit = current_fruit.split(" nutrition")
         current_fruit = new_current_fruit[0]
     fruit_names.append(current_fruit)
-print(fruit_names)
+
+# iterating through csv file to create vectors for every fruit (each component is a different nutritional value)
+nutritionalVectors = []
+for index, row, in fruits.iterrows():
+    energy = row['energy (kcal/kJ)'] # the first component is energy content
+    newEnergy = energy.split("/")
+    energy = float(newEnergy[1])
+    water = float(row['water (g)']) # the second component is water content, etc...
+    protein = float(row['protein (g)'])
+    totalFat = float(row['total fat (g)'])
+    carbs = float(row['carbohydrates (g)'])
+    calcium = float(row['calcium (mg)'])
+    iron = float(row['iron (mg)'])
+    phosphorus = float(row['phosphorus (mg)'])
+    vitaminC = float(row['vitamin C (mg)'])
+    # creating the vector based on the above components
+    nutritionalVector = np.array([energy, water, protein, totalFat, carbs, calcium, iron, phosphorus, vitaminC])
+    nutritionalVectors.append(nutritionalVector) # adding the vector to list of all vectors
+print(nutritionalVectors)
